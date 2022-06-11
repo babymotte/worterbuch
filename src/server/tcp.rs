021f33f7575686a122a64a988b1a191b2b2c12d8
Michael Bachmann <mbachmann@bbmsoft.net>
2022-06-11 12:13:47 +0200

    implementing tcp subscriptions

--- src/server/tcp.rs
+++ src/server/tcp.rs
@@ -5,11 +5,15 @@ use tokio::{
     io::AsyncWriteExt,
     net::{TcpListener, TcpStream},
     spawn,
-    sync::RwLock,
+    sync::{
+        mpsc::{self, UnboundedSender},
+        RwLock,
+    },
 };
 use worterbuch::{
     codec::{
-        encode_ack_message, encode_state_message, read_message, Ack, Get, Set, State, Subscribe,
+        encode_ack_message, encode_event_message, encode_state_message, read_message, Ack, Event,
+        Get, Set, State, Subscribe,
     },
     config::Config,
     error::{DecodeError, EncodeError},
@@ -30,17 +34,30 @@ pub async fn start(worterbuch: Arc<RwLock<Worterbuch>>, config: Config) -> Resul
     }
 }
 
-async fn serve(mut client: TcpStream, worterbuch: Arc<RwLock<Worterbuch>>) -> Result<()> {
+async fn serve(client: TcpStream, worterbuch: Arc<RwLock<Worterbuch>>) -> Result<()> {
+    let (tx, mut rx) = mpsc::unbounded_channel::<Vec<u8>>();
+
+    let (mut client_read, mut client_write) = client.into_split();
+
+    spawn(async move {
+        while let Some(bytes) = rx.recv().await {
+            if let Err(e) = client_write.write_all(&bytes).await {
+                log::error!("Error sending message to client: {e}");
+                break;
+            }
+        }
+    });
+
     loop {
-        match read_message(&mut client).await {
+        match read_message(&mut client_read).await {
             Ok(Some(worterbuch::codec::Message::Get(msg))) => {
-                get(msg, worterbuch.clone(), &mut client).await?
+                get(msg, worterbuch.clone(), tx.clone()).await?
             }
             Ok(Some(worterbuch::codec::Message::Set(msg))) => {
-                set(msg, worterbuch.clone(), &mut client).await?
+                set(msg, worterbuch.clone(), tx.clone()).await?
             }
             Ok(Some(worterbuch::codec::Message::Subscribe(msg))) => {
-                subscribe(msg, worterbuch.clone(), &mut client).await
+                subscribe(msg, worterbuch.clone(), tx.clone()).await?
             }
             Ok(None) => {
                 // client disconnected
@@ -57,16 +74,22 @@ async fn serve(mut client: TcpStream, worterbuch: Arc<RwLock<Worterbuch>>) -> Re
         }
     }
 
+    // TODO clean up any subscribers created by this client from worterbuch
+
     Ok(())
 }
 
-async fn get(msg: Get, worterbuch: Arc<RwLock<Worterbuch>>, client: &mut TcpStream) -> Result<()> {
+async fn get(
+    msg: Get,
+    worterbuch: Arc<RwLock<Worterbuch>>,
+    client: UnboundedSender<Vec<u8>>,
+) -> Result<()> {
     let wb = worterbuch.read().await;
 
     let values = match wb.get_all(&msg.request_pattern) {
         Ok(values) => values,
         Err(e) => {
-            handle_store_error(e, client).await?;
+            handle_store_error(e, client.clone()).await?;
             return Ok(());
         }
     };
@@ -78,14 +101,18 @@ async fn get(msg: Get, worterbuch: Arc<RwLock<Worterbuch>>, client: &mut TcpStre
     };
 
     match encode_state_message(&response) {
-        Ok(data) => client.write_all(&data).await?,
+        Ok(data) => client.send(data)?,
         Err(e) => handle_encode_error(e, client).await?,
     }
 
     Ok(())
 }
 
-async fn set(msg: Set, worterbuch: Arc<RwLock<Worterbuch>>, client: &mut TcpStream) -> Result<()> {
+async fn set(
+    msg: Set,
+    worterbuch: Arc<RwLock<Worterbuch>>,
+    client: UnboundedSender<Vec<u8>>,
+) -> Result<()> {
     let mut wb = worterbuch.write().await;
 
     if let Err(e) = wb.set(msg.key, msg.value) {
@@ -98,21 +125,73 @@ async fn set(msg: Set, worterbuch: Arc<RwLock<Worterbuch>>, client: &mut TcpStre
     };
 
     match encode_ack_message(&response) {
-        Ok(data) => client.write_all(&data).await?,
+        Ok(data) => client.send(data)?,
         Err(e) => handle_encode_error(e, client).await?,
     }
 
     Ok(())
 }
 
-async fn subscribe(_msg: Subscribe, _worterbuch: Arc<RwLock<Worterbuch>>, _client: &mut TcpStream) {
+async fn subscribe(
+    msg: Subscribe,
+    worterbuch: Arc<RwLock<Worterbuch>>,
+    client: UnboundedSender<Vec<u8>>,
+) -> Result<()> {
+    let mut wb = worterbuch.write().await;
+
+    let mut rx = match wb.subscribe(msg.request_pattern.clone()) {
+        Ok(rx) => rx,
+        Err(e) => {
+            handle_store_error(e, client).await?;
+            return Ok(());
+        }
+    };
+
+    let response = Ack {
+        transaction_id: msg.transaction_id,
+    };
+
+    match encode_ack_message(&response) {
+        Ok(data) => client.send(data)?,
+        Err(e) => handle_encode_error(e, client.clone()).await?,
+    }
+
+    let transaction_id = msg.transaction_id;
+    let request_pattern = msg.request_pattern;
+
+    spawn(async move {
+        while let Ok((key, value)) = rx.recv().await {
+            let event = Event {
+                transaction_id: transaction_id.clone(),
+                request_pattern: request_pattern.clone(),
+                key,
+                value,
+            };
+            match encode_event_message(&event) {
+                Ok(data) => {
+                    if let Err(e) = client.clone().send(data) {
+                        log::error!("Error sending message to client: {e}");
+                        break;
+                    }
+                }
+                Err(e) => {
+                    if let Err(e) = handle_encode_error(e, client.clone()).await {
+                        log::error!("Error sending message to client: {e}");
+                        break;
+                    }
+                }
+            }
+        }
+        // TODO unregister subscriber from worterbuch
+    });
+
+    Ok(())
+}
+
+async fn handle_encode_error(_e: EncodeError, _client: UnboundedSender<Vec<u8>>) -> Result<()> {
     todo!()
 }
 
-async fn handle_encode_error(_e: EncodeError, _client: &mut TcpStream) -> Result<()> {
-    todo!()
-}
-
-async fn handle_store_error(_e: anyhow::Error, _client: &mut TcpStream) -> Result<()> {
+async fn handle_store_error(_e: anyhow::Error, _client: UnboundedSender<Vec<u8>>) -> Result<()> {
     todo!()
 }
--- worterbuch-cli/src/bin/wbsub.rs
+++ worterbuch-cli/src/bin/wbsub.rs
@@ -1,8 +1,31 @@
 use anyhow::Result;
+use std::{env, time::Duration};
+use tokio::time::sleep;
+use worterbuch_cli::Connection;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
 
-    todo!()
+    let arg = env::args().skip(1).next();
+
+    let request_pattern = match arg {
+        Some(pattern) => pattern,
+        None => {
+            eprintln!("no subscription pattern specified");
+            return Ok(());
+        }
+    };
+
+    #[cfg(feature = "graphql")]
+    let mut con = worterbuch_cli::gql::connect().await?;
+
+    #[cfg(not(feature = "graphql"))]
+    let mut con = worterbuch_cli::tcp::connect().await?;
+
+    con.subscribe(&request_pattern)?;
+
+    loop {
+        sleep(Duration::from_secs(1)).await;
+    }
 }
