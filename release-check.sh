dd4ed70eb3ca30e7eb62108de0aa0743d84d87e6
Michael Bachmann <mbachmann@bbmsoft.net>
2022-06-30 22:49:46 +0200

    more checks in release check

--- release-check.sh
+++ release-check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cargo make test-features || exit 1
 cargo test -p libworterbuch --no-default-features || exit 1
+cargo test -p libworterbuch || exit 1
 cargo test -p libworterbuch --all-features || exit 1
 cargo test -p worterbuch --no-default-features || exit 1
+cargo test -p worterbuch || exit 1
 cargo test -p worterbuch --all-features || exit 1
 cargo test -p worterbuch-cli --all-features || exit 1
+cargo test -p worterbuch-cli || exit 1
 cargo publish -p libworterbuch --dry-run || exit 1
