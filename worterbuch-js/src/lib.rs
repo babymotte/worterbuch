c7f6b284f5e318de039af3bbe9ef41e6ea9d9fec
Michael Bachmann <mbachmann@bbmsoft.net>
2022-07-30 10:06:05 +0200

    cleanup

--- worterbuch-js/src/lib.rs
+++ worterbuch-js/src/lib.rs
@@ -1,3 +1,4 @@
+use libworterbuch::codec::{blocking::read_server_message, encode_message};
 use wasm_bindgen::prelude::*;
 
 #[global_allocator]
@@ -6,12 +7,11 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub fn encode_client_message(msg: &JsValue) -> Result<Vec<u8>, String> {
     let cm = msg.into_serde().map_err(|e| e.to_string())?;
-    libworterbuch::codec::encode_message(&cm).map_err(|e| e.to_string())
+    encode_message(&cm).map_err(|e| e.to_string())
 }
 
 #[wasm_bindgen]
 pub fn decode_server_message(data: &[u8]) -> Result<JsValue, String> {
-    libworterbuch::codec::blocking::read_server_message(&*data)
-        .map_err(|e| e.to_string())
-        .and_then(|msg| JsValue::from_serde(&msg).map_err(|e| e.to_string()))
+    let sm = read_server_message(&*data).map_err(|e| e.to_string())?;
+    JsValue::from_serde(&sm).map_err(|e| e.to_string())
 }
