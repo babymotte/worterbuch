16c8a4f90ace5ad93b27c3af86c84e06274f12c5
Michael Bachmann <mbachmann@bbmsoft.net>
2022-07-30 08:33:22 +0200

    added worterbuch-js wasm library

---
+++ worterbuch-js/README.md
@@ -0,0 +1,3 @@
+# Wörterbuch-JS
+
+A client library for [Wörterbuch](https://github.com/babymotte/worterbuch) that compiles the core Rust client library to WebAssembly so it can be used from JavaScript.
---
+++ worterbuch-js/src/lib.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn encode_client_message(msg: &JsValue) -> Result<Vec<u8>, String> {
+    let cm = msg.into_serde().map_err(|e| e.to_string())?;
+    libworterbuch::codec::encode_message(&cm).map_err(|e| e.to_string())
+}
+
+// TODO decode directly to JsValue instead of String
+#[wasm_bindgen]
+pub fn decode_server_message(data: &[u8]) -> Result<JsValue, String> {
+    libworterbuch::codec::blocking::read_server_message(&*data)
+        .map_err(|e| e.to_string())
+        .and_then(|msg| JsValue::from_serde(&msg).map_err(|e| e.to_string()))
+}
