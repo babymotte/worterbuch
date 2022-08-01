4a19440a568ee51d5bf3b415fb45db913b165100
Michael Bachmann <mbachmann@bbmsoft.net>
2022-08-01 10:11:02 +0200

    fixed example ws address

--- worterbuch-js/examples/src/index.js
+++ worterbuch-js/examples/src/index.js
@@ -19,7 +19,7 @@ async function start() {
 
 start();
 
-const url = "ws://localhost:80";
+const url = "ws://localhost:80/ws";
 const topic = "hello/world";
 
 function App() {
@@ -51,9 +51,8 @@ function App() {
     <>
       Could not connect to {url}
       <br />
-      Make sure Wörterbuch is running at localhost:80 (which is the default
-      port for websocket if not configured otherwise), e.g. by running
-      'worterbuch'.
+      Make sure Wörterbuch is running at localhost:80 (which is the default port
+      for websocket if not configured otherwise), e.g. by running 'worterbuch'.
       <p />
       If you haven't installed Wörterbuch yet, do so by running 'cargo install
       --path worterbuch &amp;&amp; cargo install --path worterbuch-cli' from the
@@ -69,7 +68,7 @@ function App() {
         Connected to {url}
         <br />
         Publish messages to topic {topic} (e.g. using 'wbset hello/world="Hello,
-        there!"')to see live updates here:
+        there!"') to see live updates here:
         <p />
         {topic} = {value}
       </div>
