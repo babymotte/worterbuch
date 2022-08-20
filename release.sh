87431f3505326cdef2f08439f3c6fd90c41e574a
Michael Bachmann <mbachmann@bbmsoft.net>
2022-08-20 10:19:12 +0200

    release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cargo make test || exit 1
+cargo ws version || exit 1
+cd worterbuch-codec && cargo make publish
+cd ../worterbuch-common && cargo make publish
+cd ../worterbuch-client && cargo make publish
+cd ../worterbuch && cargo make publish
+cd ../worterbuch-cli && cargo make publish
+cd ../worterbuch-js && cargo make publish
