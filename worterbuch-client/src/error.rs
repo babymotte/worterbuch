41109b6bc5d5b40ce1b41054ba5a685da07ec60d
Michael Bachmann <mbachmann@bbmsoft.net>
2022-12-10 11:19:34 +0100

    SubscriptionError implements Error

--- worterbuch-client/src/error.rs
+++ worterbuch-client/src/error.rs
@@ -1,7 +1,21 @@
+use std::fmt;
+
 use tokio::sync::broadcast;
 use worterbuch_common::error::Err;
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum SubscriptionError {
     RecvError(broadcast::error::RecvError),
     ServerError(Err),
 }
+
+impl fmt::Display for SubscriptionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SubscriptionError::RecvError(e) => e.fmt(f),
+            SubscriptionError::ServerError(e) => e.fmt(f),
+        }
+    }
+}
+
+impl std::error::Error for SubscriptionError {}
