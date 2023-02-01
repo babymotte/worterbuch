ab5f8c87f99e4e05ce52647d3b12c85d99ffc14e
Michael Bachmann <mbachmann@bbmsoft.net>
2023-02-01 13:37:44 +0100

    fixed release script

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cargo make test || exit 1
 cargo ws version || exit 1
-cd worterbuch-codec && cargo make publish
-cd ../worterbuch-common && cargo make publish
+cd worterbuch-common && cargo make publish
 cd ../worterbuch-client && cargo make publish
 cd ../worterbuch && cargo make publish
 cd ../worterbuch-cli && cargo make publish
-cd ../worterbuch-wasm && cargo make publish
