c68e63e8748fd597122ad8506c333eb4b8119b30
Michael Bachmann <mbachmann@bbmsoft.net>
2023-06-30 15:42:44 +0200

    improving openapi

--- worterbuch/src/server/poem/mod.rs
+++ worterbuch/src/server/poem/mod.rs
@@ -2,8 +2,13 @@ use crate::{Config, Worterbuch};
 use poem::http::StatusCode;
 use poem::Result;
 use poem::{listener::TcpListener, Route};
-use poem_openapi::{param::Query, payload::PlainText, OpenApi, OpenApiService};
+use poem_openapi::{
+    param::Query,
+    payload::{Form, PlainText},
+    OpenApi, OpenApiService,
+};
 use serde_json::json;
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 use worterbuch_common::error::WorterbuchError;
@@ -24,35 +29,81 @@ impl Api {
                 let json = serde_json::to_string(&kvp).expect("cannot fail");
                 Ok(PlainText(json))
             }
-            Err(e) => match e {
-                WorterbuchError::IllegalMultiWildcard(_)
-                | WorterbuchError::IllegalWildcard(_)
-                | WorterbuchError::MultiWildcardAtIllegalPosition(_)
-                | WorterbuchError::NoSuchValue(_)
-                | WorterbuchError::ReadOnlyKey(_) => {
-                    Err(poem::Error::new(e, StatusCode::BAD_REQUEST))
-                }
-                e => Err(poem::Error::new(e, StatusCode::INTERNAL_SERVER_ERROR)),
-            },
+            Err(e) => to_error_response(e),
+        }
+    }
+
+    #[oai(path = "/pget", method = "get")]
+    async fn pget(&self, pattern: Query<String>) -> Result<PlainText<String>> {
+        let wb = self.worterbuch.read().await;
+        match wb.pget(&pattern.0) {
+            Ok(kvps) => {
+                let json = serde_json::to_string(&kvps).expect("cannot fail");
+                Ok(PlainText(json))
+            }
+            Err(e) => to_error_response(e),
         }
     }
 
     #[oai(path = "/set", method = "post")]
-    async fn set(&self, key: Query<String>, value: Query<String>) -> Result<PlainText<String>> {
+    async fn set(&self, kvps: Form<HashMap<String, String>>) -> Result<PlainText<String>> {
         let mut wb = self.worterbuch.write().await;
-        match wb.set(key.0, json!(value.0)) {
-            Ok(()) => Ok(PlainText("Ok".to_owned())),
-            Err(e) => match e {
-                WorterbuchError::IllegalMultiWildcard(_)
-                | WorterbuchError::IllegalWildcard(_)
-                | WorterbuchError::MultiWildcardAtIllegalPosition(_)
-                | WorterbuchError::NoSuchValue(_)
-                | WorterbuchError::ReadOnlyKey(_) => {
-                    Err(poem::Error::new(e, StatusCode::BAD_REQUEST))
-                }
-                e => Err(poem::Error::new(e, StatusCode::INTERNAL_SERVER_ERROR)),
-            },
+        println!("{kvps:?}");
+        for (key, value) in kvps.0 {
+            match wb.set(key, json!(value)) {
+                Ok(()) => {}
+                Err(e) => return to_error_response(e),
+            }
         }
+        Ok(PlainText("Ok".to_owned()))
+    }
+
+    #[oai(path = "/delete", method = "post")]
+    async fn delete(&self, key: Query<String>) -> Result<PlainText<String>> {
+        let mut wb = self.worterbuch.write().await;
+        match wb.delete(key.0) {
+            Ok(kvp) => {
+                let kvp: KeyValuePair = kvp.into();
+                let json = serde_json::to_string(&kvp).expect("cannot fail");
+                Ok(PlainText(json))
+            }
+            Err(e) => to_error_response(e),
+        }
+    }
+
+    #[oai(path = "/pdelete", method = "post")]
+    async fn pdelete(&self, pattern: Query<String>) -> Result<PlainText<String>> {
+        let mut wb = self.worterbuch.write().await;
+        match wb.pdelete(pattern.0) {
+            Ok(kvps) => {
+                let json = serde_json::to_string(&kvps).expect("cannot fail");
+                Ok(PlainText(json))
+            }
+            Err(e) => to_error_response(e),
+        }
+    }
+
+    #[oai(path = "/ls", method = "get")]
+    async fn ls(&self, pattern: Query<Option<String>>) -> Result<PlainText<String>> {
+        let wb = self.worterbuch.read().await;
+        match wb.ls(pattern.0) {
+            Ok(kvps) => {
+                let json = serde_json::to_string(&kvps).expect("cannot fail");
+                Ok(PlainText(json))
+            }
+            Err(e) => to_error_response(e),
+        }
+    }
+}
+
+fn to_error_response(e: WorterbuchError) -> Result<PlainText<String>> {
+    match e {
+        WorterbuchError::IllegalMultiWildcard(_)
+        | WorterbuchError::IllegalWildcard(_)
+        | WorterbuchError::MultiWildcardAtIllegalPosition(_)
+        | WorterbuchError::NoSuchValue(_)
+        | WorterbuchError::ReadOnlyKey(_) => Err(poem::Error::new(e, StatusCode::BAD_REQUEST)),
+        e => Err(poem::Error::new(e, StatusCode::INTERNAL_SERVER_ERROR)),
     }
 }
 
@@ -67,16 +118,20 @@ pub async fn start(
 
     let api = Api { worterbuch };
 
-    let api_service = OpenApiService::new(api, "Worterbuch", env!("CARGO_PKG_VERSION"))
-        .server("http://localhost:8080/api");
+    let host_addr = "http://localhost:8080/api";
 
-    let swagger = api_service.swagger_ui();
+    let api_service =
+        OpenApiService::new(api, "Worterbuch", env!("CARGO_PKG_VERSION")).server(host_addr);
+
+    log::info!("Starting openapi service at {}", host_addr);
+
+    let openapi_explorer = api_service.openapi_explorer();
     let spec_json = api_service.spec_endpoint();
     let spec_yaml = api_service.spec_endpoint_yaml();
 
     let app = Route::new()
         .nest("/api", api_service)
-        .nest("/doc", swagger)
+        .nest("/doc", openapi_explorer)
         .nest("/json", spec_json)
         .nest("/yaml", spec_yaml);
 
