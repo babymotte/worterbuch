c665419b75ae464f5d13e17cc8d69322c689ad46
Michael Bachmann <mbachmann@bbmsoft.net>
2023-08-03 22:21:09 +0200

    publishing crates from github actions

--- release.sh
+++ release.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cargo make test || exit 1
 cargo ws version || exit 1
-cd worterbuch-common && cargo make publish
-cd ../worterbuch-client && cargo make publish
-cd ../worterbuch && cargo make publish
-cd ../worterbuch-cli && cargo make publish
