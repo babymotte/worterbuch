eff9743642a48979761c6d43a47a3764aada8fb5
Michael Bachmann <mbachmann@bbmsoft.net>
2023-08-03 23:02:37 +0200

    avoiding failure because of publish

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd worterbuch-common && cargo publish --token $CRATES_TOKEN || echo "already published"
+cd ../worterbuch-client && cargo publish --token $CRATES_TOKEN || echo "already published"
+cd ../worterbuch && cargo publish --token $CRATES_TOKEN || echo "already published"
+cd ../worterbuch-cli && cargo publish --token $CRATES_TOKEN || echo "already published"
