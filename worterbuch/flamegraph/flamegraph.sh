f26dc2271fe972b41f30691f49869545e55b8b15
Michael Bachmann <mbachmann@bbmsoft.net>
2023-09-21 18:57:14 +0200

    minor flamegraph changes

--- worterbuch/flamegraph/flamegraph.sh
+++ worterbuch/flamegraph/flamegraph.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 cargo build --release
-flamegraph -- "../../target/release/worterbuch" &
-
 sleep 1
+
+flamegraph -- "../../target/release/worterbuch" &
+sleep 3
 
 export WORTERBUCH_HOST_ADDRESS=localhost
 export WORTERBUCH_PORT=8080
 
-for i in {0..99}; do wbpsub '#' &>/dev/null & done
+for i in {0..99}; do wbpsub '#' &>/dev/null &;done
 time for i in {0..10}; do jq <"../benches/dump.json" -c '.keyValuePairs[]' | wbset -j >/dev/null; done
 
 sleep 3
-killall -s SIGINT "flamegraph"
+killall -s SIGINT "../../target/release/worterbuch"
+echo
