a392fb05261bd5fecca4063f995c2a69487690fe
Michael Bachmann <mbachmann@bbmsoft.net>
2023-10-11 12:18:50 +0200

    fixed typo

--- worterbuch/flamegraph/flamegraph.sh
+++ worterbuch/flamegraph/flamegraph.sh
@@ -9,7 +9,7 @@ sleep 3
 export WORTERBUCH_HOST_ADDRESS=localhost
 export WORTERBUCH_PORT=8080
 
-for i in {0..99}; do wbpsub '#' &>/dev/null &;done
+for i in {0..99}; do wbpsub '#' &>/dev/null & done
 time for i in {0..10}; do jq <"../benches/dump.json" -c '.keyValuePairs[]' | wbset -j >/dev/null; done
 
 sleep 3
