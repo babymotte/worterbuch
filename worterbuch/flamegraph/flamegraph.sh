68673ca9103c6a667ee5ab122c3b064932afa48b
Michael Bachmann <mbachmann@bbmsoft.net>
2023-10-12 16:57:43 +0200

    improved flamegraph script

--- worterbuch/flamegraph/flamegraph.sh
+++ worterbuch/flamegraph/flamegraph.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-cargo build --release
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+CLI_DIR="$SCRIPT_DIR/../../worterbuch-cli"
+BENCH_DIR="$SCRIPT_DIR/../benches"
+TARGET_DIR="$SCRIPT_DIR/../../target/release"
+
+cd "$CLI_DIR" && cargo build --release || exit 1
+cd "$SCRIPT_DIR" && cargo build --release || exit 1
+
+VALUES=$(jq <"$BENCH_DIR/dump.json" '.keyValuePairs | length')
+echo Values in dump: $VALUES
+
 sleep 1
-
-flamegraph -- "../../target/release/worterbuch" &
+flamegraph -- "$TARGET_DIR/worterbuch" &
 sleep 3
 
+export WORTERBUCH_PROTO=tcp
 export WORTERBUCH_HOST_ADDRESS=localhost
-export WORTERBUCH_PORT=8080
+export WORTERBUCH_PORT=8081
 
-for i in {0..99}; do wbpsub '#' &>/dev/null & done
-time for i in {0..10}; do jq <"../benches/dump.json" -c '.keyValuePairs[]' | wbset -j >/dev/null; done
+for i in {0..99}; do
+    "$TARGET_DIR/wbpsub" -j '#' &>/dev/null &
+done
+
+time for i in {0..10}; do
+    jq <"$BENCH_DIR/dump.json" -c '.keyValuePairs[]' | "$TARGET_DIR/wbset" -j >/dev/null
+done
 
 sleep 3
-killall -s SIGINT "../../target/release/worterbuch"
+killall -s SIGINT "$TARGET_DIR/worterbuch"
 echo
