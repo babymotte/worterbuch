4b1659811064b9bca5d28bc165794e1e009b2ecf
Michael Bachmann <mbachmann@bbmsoft.net>
2023-10-19 13:42:45 +0200

    using jemalloc on supported platforms

--- worterbuch/src/main.rs
+++ worterbuch/src/main.rs
@@ -1,15 +1,20 @@
-use std::time::Duration;
-
 use anyhow::Result;
 use clap::Parser;
+use std::time::Duration;
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
 use tokio_graceful_shutdown::Toplevel;
 use worterbuch::run_worterbuch;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[derive(Parser)]
 #[command(author, version, about = "An in-memory data base / message broker hybrid", long_about = None)]
 struct Args {}
 
-#[tokio::main]
+#[tokio::main()]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
