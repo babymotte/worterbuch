d7e89094ba000776d72ac97d3858b901a9ff000b
Michael Bachmann <mbachmann@bbmsoft.net>
2025-02-11 15:17:29 +0100

    added test scripts for five node cluster

---
+++ run-dave.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+RUST_LOG=info WORTERBUCH_DATA_DIR=./data/dave ./target/release/worterbuch-cluster-orchestrator -p 1340 dave
+# RUST_LOG=info WORTERBUCH_DATA_DIR=./data/carl cargo run -- -p 1339 carl
+# docker run --rm --name wbco-carl -v $(pwd)/data/carl:/data -v $(pwd)/config.yaml:/cfg/config.yaml -p 1339:1339 wbco -p 1339 carl
