c92957566dc71df8f9e52fbc974817c6aa463976
Michael Bachmann <mbachmann@bbmsoft.net>
2025-02-28 09:49:38 +0100

    updated docker run scripts

--- run-alice.sh
+++ run-alice.sh
@@ -2,4 +2,4 @@
 
 # RUST_LOG=info WORTERBUCH_DATA_DIR=./data/alice ./target/release/worterbuch-cluster-orchestrator -p 1337 -q 1 alice
 RUST_LOG=info WORTERBUCH_DATA_DIR=./data/alice cargo run -- -p 1337 --stats-port 8383 alice
-# docker run --rm --name wbco -v $(pwd)/wb-data:/data -v $(pwd)/config.yaml:/cfg/config.yaml -p 80:80 -p 9090:9090 -p 8282:8282 -p 9292:9292 docker.io/babymotte/worterbuch-cluster-orchestrator:latest -p 9292
+# docker run --rm --name wbco-alice -v $(pwd)/data/alice:/data -v $(pwd)/config.yaml:/cfg/config.yaml -p 80:80 -p 9090:9090 -p 8281:8282 -p 1337:8181/udp docker.io/babymotte/worterbuch-cluster-orchestrator:1.1.1 alice
--- run-carl.sh
+++ run-carl.sh
@@ -2,4 +2,4 @@
 
 # RUST_LOG=info WORTERBUCH_DATA_DIR=./data/carl ./target/release/worterbuch-cluster-orchestrator -p 1339 carl
 RUST_LOG=info WORTERBUCH_DATA_DIR=./data/carl cargo run -- -p 1339 --stats-port 8385 carl
-# docker run --rm --name wbco-carl -v $(pwd)/data/carl:/data -v $(pwd)/config.yaml:/cfg/config.yaml -p 1339:1339 wbco -p 1339 carl
+# docker run --rm --name wbco-carl -v $(pwd)/data/carl:/data -v $(pwd)/config.yaml:/cfg/config.yaml -p 80:80 -p 9090:9090 -p 8283:8282 -p 1339:8181/udp docker.io/babymotte/worterbuch-cluster-orchestrator:1.1.1 carl
