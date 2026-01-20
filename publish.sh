f241431f0074694becc3462b02e4258fa08c8ca2
Michael Bachmann <mbachmann@bbmsoft.net>
2026-01-20 21:19:08 +0100

    added cluster orchestrator to publish script

--- publish.sh
+++ publish.sh
@@ -4,3 +4,4 @@ cd worterbuch-common && cargo publish --token $CRATES_TOKEN || echo "already pub
 cd ../worterbuch-client && cargo publish --token $CRATES_TOKEN || echo "already published"
 cd ../worterbuch && cargo publish --token $CRATES_TOKEN || echo "already published"
 cd ../worterbuch-cli && cargo publish --token $CRATES_TOKEN || echo "already published"
+cd ../worterbuch-cluster-orchestrator && cargo publish --token $CRATES_TOKEN || echo "already published"
