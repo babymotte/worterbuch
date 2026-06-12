d267d6452928a6bb39f13e8538d6f4a18d029ce8
Michael Bachmann <mbachmann@bbmsoft.net>
2026-06-12 16:19:52 +0200

    added test relevant env vars to check script

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ cargo fmt --check &&
     cargo clippy -- --deny warnings &&
     cargo clippy --no-default-features -- --deny warnings &&
     cargo clippy --no-default-features --features=ws -- --deny warnings &&
-    cargo clippy --features=commercial -- --deny warnings &&
+    WORTERBUCH_PUBLIC_KEY_FILE=./tests/key.asc WORTERBUCH_LICENSE_FILE=./tests/license cargo clippy --features=commercial -- --deny warnings &&
     cargo test &&
     cargo test --no-default-features &&
-    cargo test --features=commercial
+    WORTERBUCH_PUBLIC_KEY_FILE=./tests/key.asc WORTERBUCH_LICENSE_FILE=./tests/license cargo test --features=commercial
